582440aed6ae9d6f6eb71297a3f2005fe4464a1c
mikhailyatsenko <misha.ggg@gmail.com>
2023-05-31 16:42:02 +0200

    updated backend and database changed to Postgresql

--- src/components/Create.js
+++ src/components/Create.js
@@ -2,45 +2,33 @@ import React from "react";
 import { useState } from "react";
 import env from "../env.json";
 import { motion } from "framer-motion";
-import {
-  Container,
-  Row,
-  Col,
-  Button,
-  Form,
-  Spinner,
-  Tooltip,
-  OverlayTrigger,
-} from "react-bootstrap";
+import { Container, Row, Col, Button, Form, Spinner, Tooltip, OverlayTrigger } from "react-bootstrap";
 import ContentCopyIcon from "@mui/icons-material/ContentCopy";
 import DoneAllIcon from "@mui/icons-material/DoneAll";
 
 function Create() {
-  const [url, setUrl] = useState("");
+  const [cipher, setCipher] = useState("");
   const [successClass, setSuccessClass] = useState("hide");
   const [loadClass, setLoadClass] = useState("hide");
   const [errorClass, setErrorClass] = useState("hide");
   const [formClass, setFormClass] = useState("");
-  const [clipboard, setClipboard] = useState(
-    <ContentCopyIcon fontSize="small" />
-  );
+  const [clipboard, setClipboard] = useState(<ContentCopyIcon fontSize="small" />);
   const [clipboardText, setClipboardText] = useState("Скопировать ключ");
 
   let sendData = (obj) => {
     setFormClass("hide");
     setLoadClass("");
-    fetch(env.urlBackend, {
+    fetch(env.urlBackend + "/create", {
       method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify(obj),
     })
       .then((response) => response.json())
       .then((response) => {
-        // console.log(response);
         if (response.result) {
           setLoadClass("hide");
           setSuccessClass("");
-          setUrl(response.url);
+          setCipher(response.cipher);
         } else {
           setLoadClass("hide");
           setErrorClass("");
@@ -54,19 +42,19 @@ function Create() {
 
   const loadDataForm = (event) => {
     event.preventDefault();
-    let note = event.target.elements.note.value;
-    note = note.trim();
-    if (note === "") {
+    let noteForEncrypt = event.target.elements.note.value;
+    noteForEncrypt = noteForEncrypt.trim();
+    if (noteForEncrypt === "") {
       alert("Заполните поле");
       return false;
     }
-    sendData({ note: note });
+    sendData({ note: noteForEncrypt });
     // console.log(note);
   };
 
   const copyToClipboard = () => {
     navigator.clipboard
-      .writeText(url)
+      .writeText(cipher)
       .then(() => {
         setClipboard(<DoneAllIcon fontSize="small" />);
         setClipboardText("Скопировано!");
@@ -77,35 +65,19 @@ function Create() {
   };
 
   return (
-    <motion.section
-      id="create"
-      className="bg-color-main height85 d-flex align-items-center"
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 0.5 }}
-    >
+    <motion.section id="create" className="bg-color-main height85 d-flex align-items-center" initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5 }}>
       <Container>
         <Row className="justify-content-center">
           <div className={formClass + " text-center"}>
-            <h2 className="fw-light">
-              Напишите заметку, которую хотите зашифровать:
-            </h2>
+            <h2 className="fw-light">Напишите заметку, которую хотите зашифровать:</h2>
           </div>
 
           <div className={formClass + " col-md-6 text-center"}>
             <Form onSubmit={loadDataForm}>
-              <Form.Group
-                className="mb-3"
-                controlId="exampleForm.ControlTextarea1"
-              >
+              <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
                 <Form.Control as="textarea" name="note" rows={3} required />
               </Form.Group>
-              <Button
-                type="submit"
-                className="fw-light"
-                variant="outline-dark"
-                size="lg"
-              >
+              <Button type="submit" className="fw-light" variant="outline-dark" size="lg">
                 Зашифровать
               </Button>
             </Form>
@@ -117,13 +89,10 @@ function Create() {
             <h2 className="fw-light mb-3">Сохраните Ваш ключ дешифровки:</h2>
             <div className="mb-4 col-md-6 alert alert-info mx-auto d-flex justify-content-center">
               <div className="me-3">
-                <strong>{url} </strong>
+                <strong>{cipher} </strong>
               </div>
               <div className="clipboard" onClick={copyToClipboard}>
-                <OverlayTrigger
-                  overlay={<Tooltip>{clipboardText}</Tooltip>}
-                  placement="top"
-                >
+                <OverlayTrigger overlay={<Tooltip>{clipboardText}</Tooltip>} placement="top">
                   {clipboard}
                 </OverlayTrigger>
               </div>
--- src/components/Note.js
+++ src/components/Note.js
@@ -1,11 +1,9 @@
 import { useState } from "react";
 import { Container, Row, Form, Button, Spinner } from "react-bootstrap";
-// import { useParams } from "react-rout   er-dom";
 import { motion } from "framer-motion";
 import env from "../env.json";
 
 function Note() {
-  // let {noteUrl} = useParams();
   const [noteText, setNoteText] = useState("");
   const [noteClass, setNoteClass] = useState("hide");
   const [loadClass, setLoadClass] = useState("hide");
@@ -13,62 +11,42 @@ function Note() {
   const [errorClass, setErrorClass] = useState("hide");
   const [noHashClass, setNoHashClass] = useState("hide");
 
-  // useEffect(()=>{
   let sendData = (obj) => {
     setFormClass("hide");
     setLoadClass("");
 
-    // if (noteUrl !== undefined) {
-    fetch(env.urlBackend, {
+    fetch(env.urlBackend + "/getnote", {
       method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify(obj),
     })
       .then((response) => response.json())
       .then((response) => {
-        console.log(response.text);
         if (response.result) {
           setNoteText(response.note);
           setLoadClass("hide");
           setNoteClass("");
-        } else if (response.text == "note not found") {
-          setLoadClass("hide");
-          setNoHashClass("");
         } else {
-          setLoadClass("hide");
-          setErrorClass("");
+          if (response.note === "note not found") {
+            setLoadClass("hide");
+            setNoHashClass("");
+          } else {
+            setLoadClass("hide");
+            setErrorClass("");
+          }
         }
       });
-
-    // }
   };
-  // else {
-  //   setLineClass('hide');
-  //   setFormClass('');
-  //   setErrorClass('hide');
-  // }
-  // }, []);
 
   let getNote = (event) => {
     event.preventDefault();
-    let url = event.target.elements.url.value;
-    // if (url === '') {
-    //   alert('Заполните поле');
-    //   return false;
-    // }
-    sendData({ url: url });
-    // noteUrl = url;
-    // window.location.href = env.url + '/' + url;
+    let cipher = event.target.elements.cipher.value;
+
+    sendData({ cipher: cipher });
   };
 
   return (
-    <motion.section
-      id="create"
-      className="bg-color-main height85 d-flex align-items-center"
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 0.5 }}
-    >
+    <motion.section id="create" className="bg-color-main height85 d-flex align-items-center" initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5 }}>
       <Container>
         <Row className="justify-content-center">
           <div className={formClass + " text-center"}>
@@ -77,18 +55,10 @@ function Note() {
 
           <div className={formClass + " col-md-6 text-center"}>
             <Form onSubmit={getNote}>
-              <Form.Group
-                className="mb-3"
-                controlId="exampleForm.ControlTextarea1"
-              >
-                <Form.Control as="input" name="url" required />
+              <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                <Form.Control as="input" name="cipher" required />
               </Form.Group>
-              <Button
-                type="submit"
-                className="fw-light"
-                variant="outline-dark"
-                size="lg"
-              >
+              <Button type="submit" className="fw-light" variant="outline-dark" size="lg">
                 Расшифровать заметку
               </Button>
             </Form>
