426db49d7a610b8af871e611f5643c132c5f3bbe
mikhailyatsenko <misha.ggg@gmail.com>
2024-04-19 17:45:35 +0200

    fix vite config for gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 import dotenv from 'dotenv';
-import react from '@vitejs/plugin-react'
-import tsconfigPaths from "vite-tsconfig-paths"
-
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 dotenv.config();
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   define: {
-    'process.env': process.env
-  }
-})
+    'process.env': process.env,
+  },
+  base: '/encryptnotes/',
+});
