d06204bfa10107a9eebef3f7847fc1b778e92a7b
mikhailyatsenko <misha.ggg@gmail.com>
2024-04-20 13:59:07 +0200

    fix preloader with lazy load

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,39 +1,41 @@
 import { Navbar } from 'widgets/Navbar';
 import AppRouter from './providers/router/AppRouter';
 import { Footer } from 'widgets/Footer';
-import { useEffect, useState } from 'react';
-import Spinner from 'shared/ui/Spinner/Spinner';
+// import { useEffect, useState } from 'react';
+// import Spinner from 'shared/ui/Spinner/Spinner';
 
 const App = () => {
-  const [loading, setLoading] = useState(true);
+  // const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const handleLoad = () => {
-      setLoading(false);
-    };
+  // useEffect(() => {
+  //   console.log(loading, 'in useeffect');
+  //   const handleLoad = () => {
+  //     setLoading(false);
+  //     console.log(loading, 'in handleLoad');
+  //   };
 
-    window.addEventListener('load', handleLoad);
+  //   window.addEventListener('load', handleLoad);
 
-    return () => {
-      window.removeEventListener('load', handleLoad);
-    };
-  }, []);
-
-  return loading ? (
-    <Spinner />
-  ) : (
-    <>
-      <nav>
-        <Navbar />
-      </nav>
-      <main>
-        <AppRouter />
-      </main>
-      <footer>
-        <Footer />
-      </footer>
-    </>
-  );
+  //   return () => {
+  //     window.removeEventListener('load', handleLoad);
+  //   };
+  // }, [loading]);
+  // console.log(loading, 'outside useeffect');
+  // return loading ? (
+  //   <Spinner />
+  // ) : (
+  <>
+    <nav>
+      <Navbar />
+    </nav>
+    <main>
+      <AppRouter />
+    </main>
+    <footer>
+      <Footer />
+    </footer>
+  </>;
+  // );
 };
 
 export default App;
