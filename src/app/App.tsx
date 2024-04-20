22045df93edd43a257565c8d731d8ce4dd591c11
mikhailyatsenko <misha.ggg@gmail.com>
2024-04-20 15:12:42 +0200

    Revert "remove lazy load"
    This reverts commit f0dfdb8465fb774959d66bbd3238ee9a49652f67.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,21 +1,22 @@
 import { Navbar } from 'widgets/Navbar';
 import AppRouter from './providers/router/AppRouter';
 import { Footer } from 'widgets/Footer';
+// import { useEffect, useState } from 'react';
+// import Spinner from 'shared/ui/Spinner/Spinner';
 
 const App = () => {
-  return (
-    <>
-      <nav>
-        <Navbar />
-      </nav>
-      <main>
-        <AppRouter />
-      </main>
-      <footer>
-        <Footer />
-      </footer>
-    </>
-  );
+  <>
+    <nav>
+      <Navbar />
+    </nav>
+    <main>
+      <AppRouter />
+    </main>
+    <footer>
+      <Footer />
+    </footer>
+  </>;
+  // );
 };
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import ReactDOM from 'react-dom/client';
-import App from 'app/App';
+// import App from 'app/App';
+import Spinner from 'shared/ui/Spinner/Spinner';
 import './index.scss';
 import { HashRouter } from 'react-router-dom';
+import { Suspense, lazy } from 'react';
+
+// eslint-disable-next-line
+// @ts-expect-error
+const AppLazy = lazy(async () => await import('app/App'));
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <HashRouter>
-    <App />
+    <Suspense fallback={<Spinner />}>
+      <AppLazy />
+    </Suspense>
   </HashRouter>,
 );
