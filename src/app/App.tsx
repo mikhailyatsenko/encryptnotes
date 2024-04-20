df62ea83b66c5a85b3122c087442ecfcef30bc65
mikhailyatsenko <misha.ggg@gmail.com>
2024-04-20 15:12:00 +0200

    fix app

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,18 +3,19 @@ import AppRouter from './providers/router/AppRouter';
 import { Footer } from 'widgets/Footer';
 
 const App = () => {
-  <>
-    <nav>
-      <Navbar />
-    </nav>
-    <main>
-      <AppRouter />
-    </main>
-    <footer>
-      <Footer />
-    </footer>
-  </>;
-  // );
+  return (
+    <>
+      <nav>
+        <Navbar />
+      </nav>
+      <main>
+        <AppRouter />
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </>
+  );
 };
 
 export default App;
