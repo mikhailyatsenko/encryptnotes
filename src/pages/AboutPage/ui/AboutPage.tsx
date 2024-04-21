bf5028ed1d87c4e62ffd5bcb5f71db2e7caa2cd6
mikhailyatsenko <misha.ggg@gmail.com>
2024-04-21 12:51:31 +0200

    fix styles about page

--- src/pages/AboutPage/ui/AboutPage.tsx
+++ src/pages/AboutPage/ui/AboutPage.tsx
@@ -7,13 +7,13 @@ export const AboutPage = () => {
         <div className={cls.AboutPage}>
           <h2>Encrypt notes is a pet project.</h2>
           <p>The service is hosted on a free server, which does not always work quickly.</p>
-          <p>Backend source code:</p>
+          <h3>Backend source code:</h3>
           <strong>
             <a href="https://github.com/mikhailyatsenko/encryptnotes_backend" target="_blank" rel="noopener noreferrer">
               github.com/mikhailyatsenko/encryptnotes_backend
             </a>
           </strong>
-          <p>Frontend source code:</p>
+          <h3>Frontend source code:</h3>
           <strong>
             <a href="https://github.com/mikhailyatsenko/encryptnotes" target="_blank" rel="noopener noreferrer">
               github.com/mikhailyatsenko/encryptnotes
